4820396739ea254936a7a571fbf5c0ec1fe29a98
githussein <mo.samy.hussein@gmail.com>
2022-10-07 16:31:47 +0200

    adds font manager file

---
+++ lib/presentation/font_manager.dart
@@ -0,0 +1,11 @@
+class FontConstant {
+  static const String fontFamily = 'Ubuntu';
+}
+
+class FontSize {
+  static const double s12 = 12;
+  static const double s14 = 14;
+  static const double s16 = 16;
+  static const double s18 = 18;
+  static const double s20 = 20;
+}
