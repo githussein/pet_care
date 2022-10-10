1ee8870f245e1a0847310c23e5e5019d9b61a6f6
githussein <mo.samy.hussein@gmail.com>
2022-10-10 21:12:30 +0200

    adds assets for onboarding view

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -2,4 +2,8 @@ const String imagePath = 'assets/images';
 
 class ImageAssets {
   static const String splashPhoto = '$imagePath/placeholder.png';
+
+  static const String dogIcon = '$imagePath/dog.svg';
+  static const String scheduleIcon = '$imagePath/schedule.svg';
+  static const String chartICon = '$imagePath/chart.svg';
 }
--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,4 +1,16 @@
 class StringManager {
   static const String undefinedRoute = 'We are lost here!';
   static const String errorMessage = 'Something wrong happened!';
+
+  static const String onboardingTitle1 = 'Your pet needs your attension';
+  static const String onboardingTitle2 =
+      'Schedule their meds and other activities';
+  static const String onboardingTitle3 =
+      'Track the treatment history for you pet';
+  static const String onboardingSubtitle1 =
+      'Your commitment is a key for a succesful treatment';
+  static const String onboardingSubtitle2 =
+      'Set reminders and add dosage details';
+  static const String onboardingSubtitle3 =
+      'See how things worked out for previous plans';
 }
